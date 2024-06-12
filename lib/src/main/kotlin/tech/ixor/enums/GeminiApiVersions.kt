39824010acbc77e7524e595b06c655a8731b8575
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-06-12 15:55:55 -0400

    feat: Add API Version and Model enum for Gemini

---
+++ lib/src/main/kotlin/tech/ixor/enums/GeminiApiVersions.kt
@@ -0,0 +1,6 @@
+package tech.ixor.enums
+
+enum class GeminiApiVersions(val versionUrl: String) {
+    V1("v1"),
+    V1_BETA("v1beta"),
+}
---
+++ lib/src/main/kotlin/tech/ixor/enums/GeminiModels.kt
@@ -0,0 +1,8 @@
+package tech.ixor.enums
+
+enum class GeminiModels(val modelCode: String) {
+    GEMINI_1_0_PRO("gemini-1.0-pro"),
+    GEMINI_1_0_PRO_VISION("gemini-pro-vision"),
+    GEMINI_1_5_FLASH("gemini-1.5-flash"),
+    GEMINI_1_5_PRO("gemini-1.5-pro"),
+}
