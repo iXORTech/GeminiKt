ca1eee5d296b10ae0b26a7e0e3756ea86a1d6178
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-06-12 16:00:41 -0400

    chore: Add some documentation

--- lib/src/main/kotlin/tech/ixor/enums/GeminiApiVersions.kt
+++ lib/src/main/kotlin/tech/ixor/enums/GeminiApiVersions.kt
@@ -1,5 +1,12 @@
 package tech.ixor.enums
 
+/**
+ * The available versions of the Gemini API.
+ * Currently having:
+ * - v1
+ * - v1beta
+ * @property versionUrl The URL path for the version.
+ */
 enum class GeminiApiVersions(val versionUrl: String) {
     V1("v1"),
     V1_BETA("v1beta"),
--- lib/src/main/kotlin/tech/ixor/enums/GeminiModels.kt
+++ lib/src/main/kotlin/tech/ixor/enums/GeminiModels.kt
@@ -1,5 +1,14 @@
 package tech.ixor.enums
 
+/**
+ * The available Google Gemini models to use in the API.
+ * Currently having:
+ * - gemini-1.0-pro
+ * - gemini-pro-vision
+ * - gemini-1.5-flash
+ * - gemini-1.5-pro
+ * @property modelCode The model code for the Gemini model.
+ */
 enum class GeminiModels(val modelCode: String) {
     GEMINI_1_0_PRO("gemini-1.0-pro"),
     GEMINI_1_0_PRO_VISION("gemini-pro-vision"),
